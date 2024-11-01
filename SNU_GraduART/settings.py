c9387f0666b741dc7b142317584b6cf8044f8e33
jinseo <jinseo040201@snu.ac.kr>
2024-11-01 18:29:59 +0900

    add: items app

--- SNU_GraduART/settings.py
+++ SNU_GraduART/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'cart',
     'purchases',
     'refunds',
-    'image',
+    'items',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
