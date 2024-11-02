41ffd600977eadfe8000c2fddfb76da27cf467e2
jinseo <jinseo040201@snu.ac.kr>
2024-11-02 14:33:07 +0900

    Feat: 과별 페이지 작품 리스트 조회, 상세 정보 조회 view

--- items/views.py
+++ items/views.py
@@ -11,18 +11,41 @@ from django.utils import timezone
 from django.conf import settings
 
 # Supabase 클라이언트 설정
-supabase: Client = create_client(settings.SUPABASE_URL, settings.SUPABASE_SERVICE_ROLE_KEY)
+supabase: Client = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
 
 
 @api_view(['GET'])
 def get_items(request, department):
   #해당 단과대 작품들 정보 전달
-  return 0
+  try:
+    items = supabase.table('items').select('*').eq('department', department).execute()
+    for item in items.data:
+      id = item['itemID']
+      images = supabase.table('item_images').select('image_square, image_original').eq('id', id).execute
+
+      #정사각 이미지가 있을 경우에만 정사각 이미지 제공
+      if images.data[0]['image_square']:
+        item['imagePath'] = images.data[0]['image_square']
+      else:
+        item['imagePath'] = images.data[0]['image_original']
+
+    return Response(items.data, status=status.HTTP_200_OK)
+  except:
+    return Response({'error': f'작품 정보 불러오기 실패'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 @api_view(['GET'])
 def get_item_details(request, item_id):
+  try:
   #item_id를 받아 상세 정보 및 사진 제공
-  return 0
+    item_detail = supabase.table('items').select("*").eq('id', item_id).execute()
+    item_images = supabase.table('item_images').select("*").eq('id', item_id).execute()
+
+    item_detail.data['imagePath'] = item_images.data[0]
+    return Response(item_detail.data, status=status.HTTP_200_OK)
+  except:
+    return Response({'error': f'작품 상세정보 불러오기 실패'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 @api_view(['GET'])
 def search_items(request):
@@ -32,5 +55,5 @@ def search_items(request):
 
 @api_view(['GET'])
 def get_representative_items(request):
-  #과별로 메인페이지에 띄울 대표 아이템들 제공
+  #과별로 메인페이지에 띄울 대표 작품들 제공
   return 0
