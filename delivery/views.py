b7c26c15c852cdba0f254bea7d56b531e489a6a2
minseo25 <ms059333@gmail.com>
2024-11-11 23:19:52 +0900

    fix: get item_id from url, not the request body

--- delivery/views.py
+++ delivery/views.py
@@ -10,16 +10,16 @@ from django.utils import timezone
 supabase: Client = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
 
 @api_view(['GET'])
-def get_delivery_status(request):
+def get_delivery_status(request, item_id):
   try:
     # 사용자 정보와 결제 내역 가져오기
     user_id = request.user.user_id
-    item_id = request.data.get("item_id")
+    item_id = str(item_id)
 
-    # 유저 검증
-    purchased_info = supabase.table("purchased").select("user_id", "id").eq("item_id", item_id).execute()
-    if not purchased_info.data or user_id != purchased_info.data[0]['user_id']:
-      return Response("error: 권한 없음", status=status.HTTP_401_UNAUTHORIZED)
+    # 구매 내역 조회
+    purchased_info = supabase.table("purchased").select("*").eq("user_id", user_id).eq("item_id", item_id).execute()
+    if not purchased_info:
+      return Response("error: 구매 내역이 없습니다.", status=status.HTTP_400_BAD_REQUEST)
 
     # 운송장 번호 가져오기
     delivery_info = supabase.table("delivery").select("tracking_num", "courier_name").eq("purchased_id", purchased_info[0]["id"]).execute()
@@ -29,7 +29,7 @@ def get_delivery_status(request):
     # 네이버 배송조회 url
     redirect_url = f"https://search.naver.com/search.naver?query={courier_name}{tracking_num}"
 
-    return Response({redirect_url}, status=status.HTTP_200_OK)
+    return Response({"redirect_url": redirect_url}, status=status.HTTP_200_OK)
 
   except:
     return Response("error: 배송 조회 실패", status=status.HTTP_400_BAD_REQUEST)
