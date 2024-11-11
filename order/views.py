60026e1cefb03e7e2548ea22b6ab070ab1dd6180
jinseo <jinseo040201@snu.ac.kr>
2024-11-11 13:31:56 +0900

    fix: purchased, order 컬럼 이동에 따른 코드 수정

--- order/views.py
+++ order/views.py
@@ -27,9 +27,9 @@ def get_order_info(request):
     #데이터 정리
     final_result = {
       "item_id": purchased_info[0]["item_id"],
-      "payment_method": purchased_info[0]["payment_method"],
-      "card_info": purchased_info[0]["card_info"],
-      "price": purchased_info[0]["total_price"], ##purchased테이블에는 아이템 별로 로그가 찍히므로, total_price 말고 price같긴한데...
+      "payment_method": order_info[0]["payment_method"],
+      "card_info": order_info[0]["card_info"],
+      "price": purchased_info[0]["price"], ##purchased테이블에는 아이템 별로 로그가 찍히므로, total_price 말고 price같긴한데...
       "refund": purchased_info[0]["refund"],
       "created_at": purchased_info[0]["created_at"], ##결제 승인 시각
       "is_confirmed": purchased_info[0]["is_confirmed"],
