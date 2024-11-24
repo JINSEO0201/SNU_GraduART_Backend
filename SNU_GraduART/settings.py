c9b122fa7f22b8ee8c16f01dd3d258d03457d54d
JINSEO0201 <jinseo040201@snu.ac.kr>
2024-11-24 12:28:16 +0900

    add: allowed_host

--- SNU_GraduART/settings.py
+++ SNU_GraduART/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY =env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '134.185.116.221']
 
 # Application definition
 
